f01923070bd65eb00cc1ecd1fa816e2a5af5e834
Aniliky <anilmalela9@gmail.com>
2024-01-04 22:46:30 +0530

    committed

---
+++ 07-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "ERROR: :$2  FAILED"
+    exit 1
+else
+    echo "$2 SUCCESS"
+fi
+}
+if [ $ID -ne 0 ]
+then 
+    echo "run this script as root user"
+    exit 1
+else
+    echo "root user"
+fi
+yum install mysql -y
+
+VALIDATE $? "INSATLLING MYSQL"
+
+yum isntall git -y
+
+VALIDATE $? "INSTALLING GIT"
